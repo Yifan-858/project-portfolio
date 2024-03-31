1c6a1a1c2f6446b8c32b97e1094dfa8a5bf6fa4d
Yifan-858 <133569248+Yifan-858@users.noreply.github.com>
2024-03-31 17:46:38 +0200

    update [style NavBar & Hero & Tech]

--- my-react-portfolio/src/component/Hero.jsx
+++ my-react-portfolio/src/component/Hero.jsx
@@ -3,11 +3,13 @@ import { StarBubble } from "./canvas/StarBubble";
 export const Hero = () => {
   return (
     <section className="hero-section">
-      <article>
-        <h4>Hi, I&#39;m Yifan!</h4>
-        <h1>Frontend Developer</h1>
-      </article>
-      <StarBubble />
+      <div className="hero-text">
+        <article>
+          <h4>YIFAN WANG</h4>
+          <h1>Frontend Developer</h1>
+        </article>
+        <StarBubble />
+      </div>
       <div className="scroll-indicator"></div>
     </section>
   );
--- my-react-portfolio/src/component/Tech.jsx
+++ my-react-portfolio/src/component/Tech.jsx
@@ -4,22 +4,24 @@ export const Tech = () => {
   return (
     <section className="tech-section">
       <h1>Tech</h1>
-      <SkillBalls />
-      <ul>
-        <p>data from json</p>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-        <li>TypeScript</li>
-        <li>React</li>
-        <li>Redux</li>
-        <li>SASS</li>
-        <li>Node.js</li>
-        <li>Mongo DB</li>
-        <li>Three.js</li>
-        <li>Git</li>
-        <li>Figma</li>
-      </ul>
+      <div className="skill-list">
+        <SkillBalls />
+        <ul>
+          <p>data from json</p>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>TypeScript</li>
+          <li>React</li>
+          <li>Redux</li>
+          <li>SASS</li>
+          <li>Node.js</li>
+          <li>Mongo DB</li>
+          <li>Three.js</li>
+          <li>Git</li>
+          <li>Figma</li>
+        </ul>
+      </div>
     </section>
   );
 };
