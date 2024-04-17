de15532565d22cbb5702508fafc9c657c497b922
Yifan-858 <133569248+Yifan-858@users.noreply.github.com>
2024-04-17 20:00:58 +0200

    update new project descriptions

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -4,15 +4,19 @@ export const About = () => {
       <h2 className="section-title">About Me</h2>
       <div className="text-container">
         <div className="about-text">
-          <p className="hi-text">Hi there</p>
+          <p className="hi-text">Hi there,</p>
           <p>
             My journey into coding was sparked by early experiences in 3D
-            modeling and light programming, revealing the power of coding to
-            bring immersive experiences to life.
+            modeling and light programming.
           </p>
           <p>
-            I'm currently diving in the world of programming, learning new
-            technologies to become a proficient <span>Frontend Developer</span>.
+            Fascinated by the transformative potential of coding in crafting
+            immersive experiences, I am passionately immersing myself in the
+            world of programming, learning new technologies to become a
+            proficient
+            <span> Frontend Developer</span>.
+          </p>
+          <p>
             Let's connect to explore collaboration opportunities and embark on
             this exciting journey together!
           </p>
