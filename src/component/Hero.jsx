062612ebceb2ed8af23d79ea3ebdb882493a7433
Yifan-858 <133569248+Yifan-858@users.noreply.github.com>
2024-09-05 22:45:20 +0200

    Update tech stack for this website

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -7,7 +7,7 @@ export const Hero = () => {
       <div className="hero-text">
         <article>
           <h4 className="name">YIFAN WANG</h4>
-          <h1 className="job-title">Frontend Developer</h1>
+          <h1 className="job-title">Web Developer</h1>
           <div className="about-text">
             <img
               className="profile-img"
@@ -15,14 +15,17 @@ export const Hero = () => {
               alt="profile-img"
             />
             <p className="about-me first-para">
-              I am a <span>Front-end Developer</span> with a passion for UX/UI
-              design and a background in scenography design and 3D art. My goal
-              is to constantly challenge myself with new technologies and
-              skills, creating user-friendly and innovative products.
+              I am a <span>Web Developer</span> with a passion for UX/UI design
+              and a background in scenography design and 3D art. My goal is to
+              constantly challenge myself with new technologies and skills,
+              creating user-friendly and innovative products.
             </p>
             <p className="about-me">
               I really thrive in roles where I can blend and bridge my two big
               passions: design and web development.
+            </p>
+            <p className="about-me">
+              Tech stack for this website: SASS, Three.js, React
             </p>
           </div>
         </article>
